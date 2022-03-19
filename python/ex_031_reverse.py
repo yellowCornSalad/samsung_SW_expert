4599f8a9ffa7e4c5dfc95823d349b0434718e9b0
junho <zk836566@gmail.com>
2022-03-19 19:02:50 +0900

    save to new variable

--- python/ex_031_reverse.py
+++ python/ex_031_reverse.py
@@ -1,9 +1,12 @@
-T = str(input("문자열을 입력해주세요."))
+T = input("문자열을 입력해주세요.")
+arr_T = []
+arr_reverse_T = []
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-def test_case_reverse(T):
+def test_case_reverse(T, arr_T): # 역순으로 출력된 배열들을 arr_reverse_T 배열에 저장
     for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
-        return T[:: -1]
+    arr_T = T.append()
+        return arr+T = T[:: -1]
 
 def if_palindrome(T):
     if T == test_case_reverse(T):
